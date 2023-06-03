bccf507980822bc75cfe586ca907c926a8ee7876
Mcjerry <mcjerry73@gmail.com>
2023-06-03 18:22:48 +0100

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -22,8 +22,16 @@ const connect = () => {
     });
 };
 
+const corsOptions = {
+  origin: "https://mtube-api.onrender.com/api",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+//this is
+app.use(cors(corsOptions));
+
 //middlewares
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
